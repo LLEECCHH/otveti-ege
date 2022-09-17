fabbd7ae381392dd11f13cbdad7181768cf6d594
LLEECCHH <110398126+LLEECCHH@users.noreply.github.com>
2022-09-17 16:11:15 +0300

    Add files via upload

---
+++ otveti-ege/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: BlueSpy
+:license: Free For Home Use
+:copyright: (c) 2022 BlueSpy
+"""
+
+import otveti_ege.py
+
+__author__ = 'BlueSpy'
+__version__ = '0.0.1'
+__email__ = 'alexej.rutskij@gmail.com'
